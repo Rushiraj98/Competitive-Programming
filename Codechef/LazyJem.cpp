3e7f00dc2a0161f07e5e89143e1e55ed5c4406b5
Rushiraj Rajesh Chavan <rushirajrajeshchavan@gmail.com>
2021-02-27 08:38:23 +0530

    Update LazyJem.cpp
    Adhoc problem -- codechef beginner

--- Codechef/LazyJem.cpp
+++ Codechef/LazyJem.cpp
@@ -1,1 +1,24 @@
-#bruteforce
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long int T, n, b, m, ans, problems; 
+  cin>>T;
+  while(T--){
+    cin>>n>>b>>m;
+    ans = 0;
+    problems = 0;
+    while(n){
+      problems = (n+1)/2; 
+      ans += problems*m;
+      n-=problems;
+      m*=2;
+      if(n){
+        ans+=b;
+      }
+    }
+    std::cout << ans << std::endl;
+  }
+	return 0;
+}
