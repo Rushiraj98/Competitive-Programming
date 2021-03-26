e6867bd813c18f82707752f8edd7f4d8305395a3
Rushiraj Rajesh Chavan <rushirajrajeshchavan@gmail.com>
2021-03-26 12:58:56 +0530

    Create watermelon.cpp

---
+++ CodeForces/4A-Watermelon/watermelon.cpp
@@ -0,0 +1,14 @@
+Mar/26/2021 12:58UTC+5.5	rushirajchavan	4A - Watermelon	GNU C++14	Accepted	62 ms	0 KB
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n%2==0 && n>2){
+	    std::cout << "YES" << std::endl;
+	}
+	else
+	    std::cout << "NO" << std::endl;
+}
