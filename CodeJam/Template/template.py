bd6285b89a53b1d3e78ba1eb5e5f3bc66f9db16c
Rushiraj Rajesh Chavan <rushirajrajeshchavan@gmail.com>
2022-04-02 00:09:33 +0530

    template.py initial cut

---
+++ CodeJam/Template/template.py
@@ -0,0 +1,13 @@
+# TODO: Make changes and add necessary functions for fast computation.
+# Avoid for loops
+# Check if Numpy can be used
+
+def solve():
+    x = input().split()
+    return x
+
+if __name__ == '__main__':
+    T = int(input())
+    for caseIndex in range(1, T + 1):
+        print(f'Case #{caseIndex}: {solve()}')
+        #print ('Case #{}: {}'.format(caseIndex, solve()))
